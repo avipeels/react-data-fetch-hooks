bb9bdab11d186c8bc53c86a9bd5524daf7687222
Avinash P <avinashpsk@gmail.com>
2020-10-29 22:37:34 +0530

    added useReducer

--- src/Hooks/useApi.js
+++ src/Hooks/useApi.js
@@ -1,26 +1,67 @@
-import { useState, useEffect } from 'react';
+import {
+    useState,
+    useEffect,
+    useReducer
+} from 'react';
 import axios from 'axios';
+const dataFetchReducer = (state, action) => {
+    switch (action.type) {
+        case 'FETCH_INIT':
+            return {
+                ...state,
+                loading: true,
+                error: false
+            };
+        case 'FETCH_SUCCESS':
+            return {
+                ...state,
+                loading: false,
+                error: false,
+                data: action.payload
+            };
+        case 'FETCH_FAILURE':
+            return {
+                ...state,
+                loading: false,
+                error: true
+            };
+        default:
+            throw new Error();
+    }
+}
 const useApi = (initialUrl, initialData) => {
-    const [data, setData] = useState(initialData);
+
     const [url, setUrl] = useState(initialUrl);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState(false);
+
+    /**  useReducer hooks returns us 
+     * 1. State object
+     * 2. Dispatch function to alter state object. This function takes an action which has type
+     *    and optional payload. All this information can be used in actual reducer function to
+     *    distill a new state from 
+     *    a. Previous State
+     *    b. Action Type
+     *    c. Action Payload
+     */
+    const [state, dispatch] = useReducer(dataFetchReducer, {
+        loading: false,
+        error: false,
+        data: initialData
+    });
+    // Can't have async directly on useEffect. Can be done inside it.
     useEffect(() => {
         const fetchData = async () => {
-            setError(false);
-            setLoading(true);
+            dispatch({ type: 'FETCH_INIT' });
             try {
-                const result = await axios.get(url)
-                setData(result.data);
+                const result = await axios.get(url);
+                dispatch({ type: 'FETCH_SUCCESS', payload: result.data });
             } catch (err) {
-                setError(err);
+                dispatch({ type: 'FETCH_FAILURE' });
             }
-            setLoading(false);
         }
         fetchData();
     }, [url]);
 
-    return [{ data, loading, error }, setUrl];
+    return [state, setUrl];
 }
 
 export default useApi;
